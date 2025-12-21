ae51489094495af2b1ca1f4096c4ec19771d8b0c
Ion Dodon <iondodon2@gmail.com>
2025-12-21 17:45:05 +0200

    padding fixes

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 pub const ICON_SIZE: u32 = 77;
 pub const ICON_SPACING: u32 = 22;
-pub const PANEL_PADDING: u32 = 26;
-pub const HIGHLIGHT_PADDING: u32 = 12;
+pub const PANEL_PADDING: u32 = 14;
+pub const HIGHLIGHT_PADDING: u32 = 24;
 pub const CORNER_RADIUS: f32 = 19.2;
 pub const BORDER_WIDTH: f32 = 1.0;
